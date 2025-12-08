aa53c985f1defad0bbe73440f66e4a4361318a1d
archilchubinidze18 <archilchubinidze18@gmail.com>
2025-12-08 18:58:52 +0400

    1

---
+++ day10/classwork/cw2.py
@@ -0,0 +1,6 @@
+# 2)გამოიტანეთ რიცხვები 0 დან 30 მდე for loop ის დახმრებით
+i=30
+while i = 30
+    print("hello world")
+    i = i + 1
+    i = i + 2
---
+++ day10/classwork/cw3.py
@@ -0,0 +1,9 @@
+# 3)დაწერეთ  რა განსხვევბაა while loop სა და for loop ს შორის
+for loop gamoiyeneba mashin roca zustad vici ramdenger shevasrulo
+    mag for i in range(5):   #5-jer shesrulebulia
+        print(i)
+        while loop gamoiyeneba roca arici ramdenger unda shesruldes
+x = 0
+while x < 5:
+     print(x)
+     x += 1

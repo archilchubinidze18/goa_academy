6183dc1deed4fac265a297d6abc95aadc15bc932
archilchubinidze18 <archilchubinidze18@gmail.com>
2025-12-29 13:18:14 +0400

    1

--- day11/classwork/cw2.py
+++ day11/classwork/cw2.py
@@ -1,5 +1,5 @@
 #  2)მომხარებელს შემოატანინე რიცხვი, და თუ ეს რიცხვი  მეტია 15 ზე, 1 დან ან რიცხვამდე ყველა რიცხვი დაპრინტეთ  ფორ ლუპით 
-number = input('enter number: ')
+number = input('enter number:')
 if int(number) > 15:
     for i in range (1, int(number)):
         print(i)
